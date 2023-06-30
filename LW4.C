b4b7c821e047d2e712b4cf466478ec87919b831e
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-06-30 15:12:11 +0530

    Update LW4.C

--- LW4.C
+++ LW4.C
@@ -2,10 +2,16 @@
 #include<stdio.h>
 #include<conio.h>
 void main(){
-
-
+int a,b,n1,n2,j;
 clrscr();
+printf("enter any number:");
+scanf("%d",j);
 
+for (a=3,a<=b;;a++){
+printf("%d\n",j);
+n1=n2;
+n2=j;
+j=n1+n2;}
 
 
 getch();}
